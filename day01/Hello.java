575dbf14fb8c1b17eb8751ff7f24acd352029ab2
jun880381 <144294898+jun880381@users.noreply.github.com>
2024-08-06 13:12:39 +0900

    Create Hello.java

---
+++ day01/Hello.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*여러줄 주석*/
+		/**
+		 * 문서(document) 주석
+		 * 작성일: 
+		 * 작성자:
+		 * 내용 : 
+		 */
+		System.out.print("Hello Java \nHello Java \tHello Java\n");
+		System.out.println("\tHello Java");
+
+	}
+
+}
