b3f57778da740079be0f2934edb07b77acdf093f
jun880381 <144294898+jun880381@users.noreply.github.com>
2024-08-20 16:53:53 +0900

    Update UserRegister.java

--- day10/UserRegister.java
+++ day10/UserRegister.java
@@ -7,7 +7,7 @@ public class UserRegister {
 	Connection con = null;
 	PreparedStatement ps = null;
 	User user = null;
-	Scanner sc = null;
+	Scanner sc = new Scanner(System.in);
 	ResultSet rs = null;
 	ConnectDB condb = null;
 	
@@ -16,8 +16,28 @@ public class UserRegister {
 		con = condb.getCon();
 	}
 	
-	public void Register() {
+	public void Register(User user) throws SQLException {
 		int num = 0;
+		this.user = user;
+
+		String sql = "INSERT INTO users VALUES(?, ?, ?, ?, ?)";
+		ps = con.prepareStatement(sql);
+		ps.setString(1, user.getUserid());
+		ps.setString(2, user.getUsername());
+		ps.setString(3, user.getUserpassword());
+		ps.setInt(4, user.getUserage());
+		ps.setString(5, user.getUseremail());
+		
+		num = ps.executeUpdate();
+				
+		if(num > 0) {
+			System.out.println("회원 가입 성공");
+		}else {
+			System.out.println("회원 가입 실패");
+		}
+	}
+	
+	public void UserCreate() throws Exception {
 		user = new User();
 		
 		System.out.println("[회원 가입]");
@@ -27,24 +47,14 @@ public class UserRegister {
 		user.setUsername(sc.nextLine());
 		System.out.println("비밀번호 : ");
 		user.setUserpassword(sc.nextLine());
-		System.out.println("나이 : ");
-		user.setUserage(sc.nextInt());
 		System.out.println("이메일 : ");
 		user.setUseremail(sc.nextLine());
+		System.out.println("나이 : ");
+		user.setUserage(sc.nextInt());
 		
-		try {
-		String sql = "INSERT INTO users VALUES(?, ?, ?, ?, ?)";
-		ps = con.prepareStatement(sql);
-		ps.setString(1, user.getUserid());
-		ps.setString(2, user.getUsername());
-		ps.setString(3, user.getUserpassword());
-		ps.setInt(4, user.getUserage());
-		ps.setString(4, user.getUseremail());
+		Register(user);
 		
-		num = ps.executeUpdate();
-		}catch(Exception e) {
-			e.getMessage();
-		}
-		
+		BoardExample be = new BoardExample();
+		be.mainMenu();
 	}
 }
